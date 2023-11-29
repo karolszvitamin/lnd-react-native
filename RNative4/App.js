2abcee18059bfecb63420dcbd1a194ae456e4519
karolszvitamin <151042325+karolszvitamin@users.noreply.github.com>
2023-11-29 15:17:06 +0100

    Make eslint happy

--- RNative4/App.js
+++ RNative4/App.js
@@ -1,6 +1,5 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import ManageExpense from "./screens/ManageExpenses";
